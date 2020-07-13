1f30977fb77aaad73e2d5d5f95e1d50a7e36a3a8
eruong <ethan.ong@pomona.edu>
2020-07-12 22:20:36 -0700

    fixed paths

--- convert_to_csv.py
+++ convert_to_csv.py
@@ -11,9 +11,9 @@ import csv
 + Too many rows to be opened in Excel
 + Commented out after initial file conversion
 """
-# with open('parsed_messages.txt', 'r', encoding='utf-8') as in_file:
+# with open('lwc_data\parsed_messages.txt', 'r', encoding='utf-8') as in_file:
 #     stripped = (line.strip() for line in in_file)
 #     lines = (line.split(",") for line in stripped if line)
-#     with open('parsed_messages.csv', 'w', encoding='utf-8') as out_file:
+#     with open('lwc_data\parsed_messages.csv', 'w', encoding='utf-8') as out_file:
 #         writer = csv.writer(out_file, lineterminator = '\n')
 #         writer.writerows(lines)
